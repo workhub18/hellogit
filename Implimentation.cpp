8f70ed2769a5e0f60083bc2c7ff194be72b7b307
workhub18 <workhub18@users.noreply.github.com>
2018-02-12 12:03:08 -0400

    Added implimentation file

---
+++ Implimentation.cpp
@@ -0,0 +1,37 @@
+ #include<iostream>
+ #include<string>
+ #include<vector>
+ using namespace std;
+ 
+ void Enterpupil(vector<Student>&newpupil)
+ {
+	 string pupilname;
+	 int pupilid;
+ 
+ for (int a=0;0<5;a++)
+ {
+	 cout<<"Enter Student Name"<<endl;
+	 cin>>pupilname;
+	 cout<<"Enter Student Id"<<endl;
+	 cin>>pupilid;
+	 
+	 Student pupil(pupilname,pupilid);
+	 newpupil.push_back(pupil);
+ }
+ }
+ 
+ void Printpupil(const vector<Student>&newpupil)
+ {
+	 for (int a=0;a<5;a++)
+	 {
+		 cout<<"Name "<<newpupil.GetName();
+		 cout<<"Id "<<newpupil.GetId();
+	 }
+ }
+ int main()
+ {
+	 vector<student>pupil;
+	 Enterpupil(pupil);
+	 Printpupil(pupil);
+	 retrun 0;
+ }
